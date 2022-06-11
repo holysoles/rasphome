52b7ec80ca2a81dae7eb724b0fce41c12e8a44e8
holysoles <holysoles97@gmail.com>
2022-06-11 19:28:48 +0000

    safely check values

--- devices/samsungTv.js
+++ devices/samsungTv.js
@@ -4,21 +4,25 @@ var relay = new Gpio(21, 'out');
 module.exports = {
     default: function (payload) {
         var response = {};
-        switch (payload.action) {
-            case 'on':
-                console.log('received power on command for samsung tv');
-                relay.writeSync(1);
-                break;
-            case 'off':
-                console.log('received power off command for samsung tv');
-                relay.writeSync(0);
-                break;
-            case 'check_state':
-                console.log('checking state of samsung tv');
-                response.state = relay.readSync();
-                break;
-            default:
-                response.result = 'no valid action provided'
+        if(payload.action){
+            throw 'no action was provided';
+        } else {
+            switch (payload.action) {
+                case 'on':
+                    console.log('received power on command for samsung tv');
+                    relay.writeSync(1);
+                    break;
+                case 'off':
+                    console.log('received power off command for samsung tv');
+                    relay.writeSync(0);
+                    break;
+                case 'check_state':
+                    console.log('checking state of samsung tv');
+                    response.state = relay.readSync();
+                    break;
+                default:
+                    response.result = 'a valid action was not provided.'
+            }
         }
         return response;
     }
